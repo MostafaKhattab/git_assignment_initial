a761270e147bb2345b53292eb112cc1543c098a4
MostafaKhattab <57361833+MostafaKhattab@users.noreply.github.com>
2019-11-26 07:45:55 -0800

    Update CircleCircumference.java

--- src/main/java/MathTopics/CircleCircumference.java
+++ src/main/java/MathTopics/CircleCircumference.java
@@ -1,27 +1,27 @@
 package MathTopics;
 
 import git_assignment_initial.ITopic;
-
+import git_assignment_initial.PrintStyle;
+import git_assignment_initial.Topic;
 // Please read CicleArea.java Code and implement as a clone of it but using your formula
 
-public class CircleCircumference implements ITopic  {
-	 static double circumference(double r){ 
-		  
-	        double PI = 3.1415; 
-	        double cir = 2*PI*r; 
+public class CircleCircumference implements ITopic {
+	private int input;
+    public CircleCircumference(){
+    	this.input=0;
+    }
+	public static double GetCircleCircumference(int r) {
+		    double PI = 3.1415; 
+	        double cir = 2*PI*(double)r; 
 	        return cir; 
-	    } 
+	}
 
 	@Override
-	public String NotifyTopic(int r) {
-
-		
-		
-		
-		long ans = circumference(r);
-		ans=Math.round(circumference(r) * 1000) / 1000.0;
-                 String output = PrintStyle.print(input, "Circumference") + ans;
-                 return output;
+	public String NotifyTopic(Topic topic) {
+        this.input=topic.getInput();
+        double result = Math.round(GetCircleCircumference(this.input) * 1000) / 1000.0; 
+        String output = PrintStyle.print(this.input, "Circle Circumference") + result;
+        return output;
 	}
 
 }
