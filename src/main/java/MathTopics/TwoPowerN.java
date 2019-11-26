fa84fc93ea4d7dae7cdc98db20ce47f06069d8ad
merettenabil33 <merettekamel33@gmail.com>
2019-11-26 13:25:30 +0200

    TwoPowerN Done

--- src/main/java/MathTopics/TwoPowerN.java
+++ src/main/java/MathTopics/TwoPowerN.java
@@ -1,17 +1,19 @@
 package MathTopics;
-
+import git_assignment_initial.*;
 import git_assignment_initial.ITopic;
 
 //Please read CicleArea.java Code and implement as a clone of it but using your formula
 
 public class TwoPowerN implements ITopic {
 	public static long Get2PN(int n) {
-		return 32768;
+		 return (long)(2 ^ n);
 	}
 
 	@Override
 	public String NotifyTopic(int input) {
-
-		return "Input = 10\n2PN = " + 1024;
+               long ans = Get2PN(input);
+	       String output = PrintStyle.print(input, "2PN") + ans;
+	       return output;
+		
 	}
 }
